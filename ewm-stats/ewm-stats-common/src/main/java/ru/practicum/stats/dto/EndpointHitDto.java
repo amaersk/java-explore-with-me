2b9e3d7560470dadc74e04e2d84b157fefa70e40
Andrey Z. <mmaersk@gmail.com>
2026-01-27 00:23:57 +0300

    Codestyle

--- ewm-stats/ewm-stats-common/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ ewm-stats/ewm-stats-common/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -3,53 +3,51 @@ package ru.practicum.stats.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public class EndpointHitDto {
-	@NotBlank
-	private String app;
-	
-	@NotBlank
-	private String uri;
-	
-	@NotBlank
-	private String ip;
-	
-	@NotNull
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	private LocalDateTime timestamp;
+    @NotBlank
+    private String app;
+    @NotBlank
+    private String uri;
+    @NotBlank
+    private String ip;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
 
-	public String getApp() {
-		return app;
-	}
+    public String getApp() {
+        return app;
+    }
 
-	public void setApp(String app) {
-		this.app = app;
-	}
+    public void setApp(String app) {
+        this.app = app;
+    }
 
-	public String getUri() {
-		return uri;
-	}
+    public String getUri() {
+        return uri;
+    }
 
-	public void setUri(String uri) {
-		this.uri = uri;
-	}
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
 
-	public String getIp() {
-		return ip;
-	}
+    public String getIp() {
+        return ip;
+    }
 
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
 
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
 
 
