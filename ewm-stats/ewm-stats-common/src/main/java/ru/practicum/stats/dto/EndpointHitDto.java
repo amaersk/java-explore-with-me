7e3d8959a9b8d23ad60f47b7172800e27329a7a1
Andrey Z. <mmaersk@gmail.com>
2026-01-27 00:15:13 +0300

    Fixed for postman tests

--- ewm-stats/ewm-stats-common/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ ewm-stats/ewm-stats-common/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,11 +1,22 @@
 package ru.practicum.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public class EndpointHitDto {
+	@NotBlank
 	private String app;
+	
+	@NotBlank
 	private String uri;
+	
+	@NotBlank
 	private String ip;
+	
+	@NotNull
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime timestamp;
 
 	public String getApp() {
