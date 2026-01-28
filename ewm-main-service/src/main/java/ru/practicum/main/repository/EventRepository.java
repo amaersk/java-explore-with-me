375fcf48a54f04841a11c88a575305dc3562fb3c
Andrey Z. <mmaersk@gmail.com>
2026-01-28 13:26:13 +0300

    Remove IS EMPTY from JPQL query - not supported for parameters in Hibernate 6

--- ewm-main-service/src/main/java/ru/practicum/main/repository/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/main/repository/EventRepository.java
@@ -33,7 +33,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 	@Query("SELECT e FROM Event e WHERE " +
 			"e.state = 'PUBLISHED' AND " +
 			"(:text IS NULL OR :text = '' OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) AND " +
-			"(:categories IS NULL OR :categories IS EMPTY OR e.category.id IN :categories) AND " +
+			"(:categories IS NULL OR e.category.id IN :categories) AND " +
 			"(:paid IS NULL OR e.paid = :paid) AND " +
 			"(:rangeStart IS NULL OR e.eventDate >= :rangeStart) AND " +
 			"(:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) AND " +
